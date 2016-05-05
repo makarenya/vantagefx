f7165860d6ff2e570df7f0024ab1e2bb1dba6c2e
Алексей Макареня <alexey@deli.su>
2016-05-05 22:42:55 +0300

    Добавлена модель информации о опции

---
+++ src/model/OptionInfo.h
@@ -0,0 +1,17 @@
+//
+// Created by alexx on 05.05.2016.
+//
+
+#ifndef VANTAGEFX_OPTIONINFO_H
+#define VANTAGEFX_OPTIONINFO_H
+
+#include <string>
+
+struct OptionInfo {
+    std::string name;
+    std::string expires;
+    int returnValue;
+};
+
+
+#endif //VANTAGEFX_OPTIONINFO_H
